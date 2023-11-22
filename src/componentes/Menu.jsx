5612c38b1645bd89e38f831d5023d7efe1a95bd3
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2023-11-21 22:28:50 -0300

    bbbbbbbbbbbbb

--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -13,8 +13,8 @@ function Menu() {
     <Navbar expand="lg" bg="light" className="custom-navbar">
       <Navbar.Brand href="#">Gabi Makeup</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        {/* <Nav className="mr-auto"> */}
+      {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+        <Nav className="mr-auto">
           <Nav.Link as={Link} to="/home" className="menu-link">Início</Nav.Link>
           <Nav.Link as={Link} to="/sobre" className="menu-link">Sobre Mim</Nav.Link>
           <Nav.Link as={Link} to="/galeria" className="menu-link">Galeria</Nav.Link>
