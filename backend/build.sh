d53fb3017fdfccd8e173cf29a3d5f424208fe2d5
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-22 19:47:43 -0300

    Adiciona script de inicialização para configurar o Cloud SQL Auth Proxy

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,19 @@
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r backend/requirements.txt
+python backend/apisGabiMakeup/manage.py collectstatic --no-input
+python backend/apisGabiMakeup/manage.py migrate 
 
-python manage.py collectstatic --no-input
-python manage.py migrate 
+# Baixar e instalar o Cloud SQL Auth Proxy
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+
+# Criar o arquivo de credenciais a partir da variável de ambiente
+echo $GOOGLE_APPLICATION_CREDENTIALS_JSON > credentials.json
+
+# Exportar a variável de ambiente apontando para o arquivo de credenciais
+export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/credentials.json
+
+# Iniciar o Cloud SQL Auth Proxy
+./cloud_sql_proxy -dir=/cloudsql -instances=<YOUR_INSTANCE_CONNECTION_NAME> &
+
