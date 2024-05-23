c4a303328ec38b04c1465cddca4cfad3ac5cc7e9
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-23 14:08:30 -0300

    Adiciona STATIC_ROOT ao settings.py

--- backend/apisGabiMakeup/apisGabiMakeup/settings.py
+++ backend/apisGabiMakeup/apisGabiMakeup/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
