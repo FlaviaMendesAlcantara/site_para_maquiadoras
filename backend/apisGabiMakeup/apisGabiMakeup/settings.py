ba38f6b6bca0961f49fdc13129314efdaf03a89d
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-23 19:44:53 -0300

    outro ajustes configuracao do gunicorn

--- backend/apisGabiMakeup/apisGabiMakeup/settings.py
+++ backend/apisGabiMakeup/apisGabiMakeup/settings.py
@@ -70,7 +70,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
 
-ROOT_URLCONF = 'backend.apisGabiMakeup.apisGabiMakeup.urls'
+ROOT_URLCONF = 'apisGabiMakeup.urls'
 
 TEMPLATES = [
     {
@@ -88,7 +88,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.apisGabiMakeup.apisGabiMakeup.wsgi.application'
+WSGI_APPLICATION = 'apisGabiMakeup.wsgi.application'
 
 #condfiguracao do swagger
 SWAGGER_SETTINGS = {
