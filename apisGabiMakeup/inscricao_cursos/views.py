34fcc6efa60452c08da422416d0ec83c69640333
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-24 18:47:38 -0300

    adicioando

--- apisGabiMakeup/inscricao_cursos/views.py
+++ apisGabiMakeup/inscricao_cursos/views.py
@@ -1,9 +1,9 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import InscricaoCurso
-from .serializers import InscricaoCursoSerializer
+# from rest_framework import viewsets
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from .models import InscricaoCurso
+# from .serializers import InscricaoCursoSerializer
 
-class InscricaoCursoViewSet(viewsets.ModelViewSet):
-    queryset = InscricaoCurso.objects.all()
-    serializer_class = InscricaoCursoSerializer
+# class InscricaoCursoViewSet(viewsets.ModelViewSet):
+#     queryset = InscricaoCurso.objects.all()
+#     serializer_class = InscricaoCursoSerializer
     # permission_classes = [IsAuthenticatedOrReadOnly]
