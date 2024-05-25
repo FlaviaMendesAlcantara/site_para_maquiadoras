9e79631c4cb2c806c9b743654928a24ea91dbec1
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-25 15:13:19 -0300

    ainda tentando fazer deploy perfil de usuarios29

--- apisGabiMakeup/apisGabiMakeup/settings.py
+++ apisGabiMakeup/apisGabiMakeup/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'apisGabiMakeup',
-    'teste',
+    'teste.apisGabiMakeup.TesteConfig',
     # 'cursos',
     # 'perfil_usuario',
     # 'usuarios',
