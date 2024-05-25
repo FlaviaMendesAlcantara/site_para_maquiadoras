1ff99b18d941fed08765ca1369780afcd0cefe0f
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-25 16:30:12 -0300

    ainda tentando fazer deploy perfil de usuarios32

--- apisGabiMakeup/apisGabiMakeup/views.py
+++ apisGabiMakeup/apisGabiMakeup/views.py
@@ -1,6 +1,21 @@
 # apisGabiMakeup/apisGabiMakeup/views.py
 
 from django.http import JsonResponse
+from rest_framework import generics
+from .models import PerfilUsuario
+from .serializers import PerfilUsuarioSerializer
+from django.http import HttpResponse
+
+class PerfilUsuarioListCreate(generics.ListCreateAPIView):
+    queryset = PerfilUsuario.objects.all()
+    serializer_class = PerfilUsuarioSerializer
+
+class PerfilUsuarioDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PerfilUsuario.objects.all()
+    serializer_class = PerfilUsuarioSerializer
+
+def index(request):
+    return HttpResponse("Olá, este é o perfil do usuário.")
 
 def hello_world(request):
     return JsonResponse({'message': 'Hello, world!'})
