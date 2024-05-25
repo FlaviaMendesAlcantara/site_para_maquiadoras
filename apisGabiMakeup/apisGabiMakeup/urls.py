0aa0633fe20cd2a54dd8cc5a95188651c46b34d1
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-25 16:38:24 -0300

    ainda tentando fazer deploy perfil de usuarios33

--- apisGabiMakeup/apisGabiMakeup/urls.py
+++ apisGabiMakeup/apisGabiMakeup/urls.py
@@ -56,7 +56,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('admin/', admin.site.urls),
+    path('admin/', admin.apisGabiMakeup.urls),
+    path("", views.hello_world.as_view(), name="index"),
 
     # path('cursos/', CursoListCreateAPIView.as_view(), name='curso-list-create'),
     # path('cursos/<int:pk>/', CursoRetrieveUpdateDestroyAPIView.as_view(), name='curso-retrieve-update-destroy'),
