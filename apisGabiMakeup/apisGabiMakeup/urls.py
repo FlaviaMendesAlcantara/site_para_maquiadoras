591dc4292b5e0e6c1cb4a1b6a449d1fcebbda220
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-24 23:13:46 -0300

    ainda tentando fazer deploy perfil de usuarios15

--- apisGabiMakeup/apisGabiMakeup/urls.py
+++ apisGabiMakeup/apisGabiMakeup/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('perfil_usuario/', include('perfil_usuario.urls')),
+    # path('perfil_usuario/', include('perfil_usuario.urls')),
 ]
