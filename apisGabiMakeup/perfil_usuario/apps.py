99c98bbb397287d2bee4e86b3cdc27c665c4fcaf
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-24 22:00:22 -0300

    ainda tentando fazer deploy perfil de usuarios4

---
+++ apisGabiMakeup/perfil_usuario/apps.py
@@ -0,0 +1,6 @@
+# perfil_usuario/apps.py
+from django.apps import AppConfig
+
+class PerfilUsuarioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'perfil_usuario'
