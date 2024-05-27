fe64d856379671306d5ecb64aeafae0eb712004f
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-27 19:46:16 -0300

    Remover backend, manter apenas frontend

--- apisGabiMakeup/urls.py
+++
@@ -1,9 +0,0 @@
-# apisGabiMakeup/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('v1/', include('apisGabiMakeup.apisGabiMakeup.urls')),
-]
