1200e452e32c8aaf13bd0b77fc61f8d67f6f0d22
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-30 09:40:10 -0300

    atualização autenticacao  apis

--- apisGabiMakeup/apisGabiMakeup/views.py
+++ apisGabiMakeup/apisGabiMakeup/views.py
@@ -17,7 +17,6 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-
 class UsuarioListCreate(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = UsuarioSerializer
