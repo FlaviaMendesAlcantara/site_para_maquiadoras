cd285417592296554a29c831b2da93f519d1047e
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-30 10:56:21 -0300

    Atualizando permissao de usuario

--- apisGabiMakeup/apisGabiMakeup/views.py
+++ apisGabiMakeup/apisGabiMakeup/views.py
@@ -15,7 +15,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework.response import Response 
 
 class UsuarioListCreate(generics.CreateAPIView):
     # permission_classes = [IsAuthenticated]
