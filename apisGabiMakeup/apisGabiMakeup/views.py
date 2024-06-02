16fa56e5d687eee0b4d264dec04498361401dac6
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-06-02 09:43:11 -0300

    adicionando validaao autenticate

--- apisGabiMakeup/apisGabiMakeup/views.py
+++ apisGabiMakeup/apisGabiMakeup/views.py
@@ -23,26 +23,10 @@ class UsuarioListCreate(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         data = request.data.copy()
-
-        # Verificar se 'usu_usuario' e 'password' estão nos dados do request
-        if 'usu_usuario' in data and 'password' in data:
-            # Tentar autenticar o usuário com as credenciais fornecidas
-            user = authenticate(request, username=data['usu_usuario'], password=data['password'])
-
-            if user is not None:
-                # Se o usuário for autenticado com sucesso, registrar na sessão
-                login(request, user)
-                return Response({'message': 'Usuário autenticado com sucesso.'}, status=status.HTTP_200_OK)
-
-        # Validação de perfil
         perfil_padrao = PerfilUsuario.objects.filter(per_nome="usuário padrão").first()
         if not perfil_padrao:
             return Response({'error': 'Perfil padrão não encontrado.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Adicionar o perfil de usuário padrão aos dados do usuário
         data['usu_perfil'] = perfil_padrao.pk
-
-        # Validar e criar usuário
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -50,19 +34,6 @@ class UsuarioListCreate(generics.CreateAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def post(self, request, *args, **kwargs):
-        data = request.data.copy()
-
-        # Verificar se 'usu_usuario' e 'password' estão nos dados do request
-        if 'usu_usuario' in data and 'password' in data:
-            # Tentar autenticar o usuário com as credenciais fornecidas
-            user = authenticate(request, username=data['usu_usuario'], password=data['password'])
-
-            if user is not None:
-                # Se o usuário for autenticado com sucesso, registrar na sessão
-                login(request, user)
-                return Response({'message': 'Usuário autenticado com sucesso.'}, status=status.HTTP_200_OK)
-
-        # Se as credenciais não estiverem presentes ou a autenticação falhar, prosseguir com a criação do usuário
         return self.create(request, *args, **kwargs)
 
 class UsuarioDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -72,24 +43,14 @@ class UsuarioDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class UsuarioAuthenticationAPIView(APIView):
     permission_classes = [AllowAny]
+
     def post(self, request, *args, **kwargs):
-        # Recebendo os dados do request
         usu_usuario = request.data.get('usu_usuario')
         password = request.data.get('password')
 
-        # Buscando o usuário no banco de dados
-        try:
-            user = Usuario.objects.get(usu_usuario=usu_usuario)  # Change this line
-        except Usuario.DoesNotExist:  # Change this line
-            # Usuário não encontrado
-            return Response({'error': 'Usuário não encontrado.'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        # Verificando se a senha está correta
-        if user.check_password(password):
-            # Autenticando o usuário
+        user = authenticate(request, username=usu_usuario, password=password)
+        if user is not None:
             login(request, user)
-
-            # Obtendo o perfil do usuário
             perfil_usuario = user.usu_perfil
             data = {
                 'message': 'Usuário autenticado com sucesso.',
@@ -100,8 +61,8 @@ class UsuarioAuthenticationAPIView(APIView):
             }
             return Response(data, status=status.HTTP_200_OK)
         else:
-            # Senha incorreta
-            return Response({'error': 'Senha incorreta.'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'error': 'Usuário ou senha incorretos.'}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class PerfilUsuarioListCreate(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
